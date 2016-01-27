400b15c6dec4467d5dcc0fbf4c3eaaac64cdb171
Frazer McLean <frazer@frazermclean.co.uk>
2016-01-27 20:56:54 +0100

    Import AsyncSpaceTrackClient on Python 3.5

--- spacetrack/__init__.py
+++ spacetrack/__init__.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 from __future__ import absolute_import, division, print_function
 
+import sys
+
 from .base import AuthenticationError, SpaceTrackClient
 from .operators import (
     greater_than, inclusive_range, less_than, like, not_equal, startswith)
 
-__all__ = (
+__all__ = [
     'AuthenticationError',
     'greater_than',
     'inclusive_range',
@@ -14,7 +16,13 @@ __all__ = (
     'not_equal',
     'SpaceTrackClient',
     'startswith',
-)
+]
+
+if sys.version_info >= (3, 5):
+    from .aio import AsyncSpaceTrackClient
+    __all__.append('AsyncSpaceTrackClient')
+
+__all__ = tuple(__all__)
 
 __version__ = '0.1.0'
 __description__ = 'Python client for space-track.org'
