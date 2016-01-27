cb45056aeab92eb436c74337b22236fba1d2d822
Frazer McLean <frazer@frazermclean.co.uk>
2016-01-28 00:29:21 +0100

    test generic_request exceptions

--- tests/test_spacetrack.py
+++ tests/test_spacetrack.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 from __future__ import absolute_import, division, print_function
 
+import pytest
 from requests import Response
+from spacetrack import SpaceTrackClient
 from spacetrack.base import _iter_content_generator, _iter_lines_generator
 
 try:
@@ -32,3 +34,28 @@ def test_iter_content_generator():
         result = list(
             _iter_content_generator(response=Response(), decode_unicode=True))
         assert result == ['1\n2\n', '3', '\n4', '\n5']
+
+
+def test_generic_request():
+    st = SpaceTrackClient('identity', 'password')
+
+    with pytest.raises(ValueError):
+        st.generic_request(class_='tle', iter_lines=True, iter_content=True)
+
+    with pytest.raises(ValueError):
+        st.generic_request(class_='thisclassdoesnotexist')
+
+    def mock_get_predicate_fields(self, class_):
+        return set()
+
+    patch_authenticate = patch.object(SpaceTrackClient, 'authenticate')
+
+    patch_get_predicate_fields = patch.object(
+        SpaceTrackClient, '_get_predicate_fields', mock_get_predicate_fields)
+
+    with patch_authenticate, patch_get_predicate_fields:
+        with pytest.raises(TypeError):
+            st.generic_request('tle', madeupkeyword=None)
+
+
+
