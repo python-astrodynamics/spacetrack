bfaa266629f8209ef803f8749b5c53de5884b953
Frazer McLean <frazer@frazermclean.co.uk>
2016-01-28 08:58:36 +0100

    Test dynamic method

--- tests/test_spacetrack.py
+++ tests/test_spacetrack.py
@@ -10,9 +10,9 @@ from spacetrack import AuthenticationError, SpaceTrackClient
 from spacetrack.base import _iter_content_generator, _iter_lines_generator
 
 try:
-    from unittest.mock import patch
+    from unittest.mock import call, patch
 except ImportError:
-    from mock import patch
+    from mock import call, patch
 
 
 def test_iter_lines_generator():
@@ -120,6 +120,14 @@ def test_generic_request():
         '1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927',
         '2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537'
     ]
+
+
+def test_spacetrack_methods():
+    st = SpaceTrackClient('identity', 'password')
+    with patch.object(SpaceTrackClient, 'generic_request') as mock_generic_request:
+        st.tle_publish()
+        assert mock_generic_request.call_count == 1
+        assert mock_generic_request.call_args == call('tle_publish')
 
 
 @responses.activate
