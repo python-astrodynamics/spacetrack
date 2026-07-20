540749c7141c2588920eaf80035250290b552f5c
Frazer McLean <frazer@frazermclean.co.uk>
2026-07-20 13:10:40 +0100

    Add dependency-boundary test sessions

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterator
+from contextlib import contextmanager
+
 import nox
 
 nox.options.reuse_existing_virtualenvs = True
@@ -36,3 +39,42 @@ def tests(session: nox.Session) -> None:
     )
 
     session.run("coverage", "run", "-m", "pytest", *session.posargs)
+
+
+@nox.session(name="test-min-deps", python=PYTHON_VERSIONS)
+def test_min_deps(session: nox.Session) -> None:
+    with restore_file("uv.lock"):
+        session.run_install(
+            "uv",
+            "sync",
+            "--no-default-groups",
+            "--group=test",
+            "--resolution=lowest-direct",
+        )
+
+        session.run("pytest", *session.posargs)
+
+
+@nox.session(name="test-latest", python=PYTHON_VERSIONS)
+def test_latest(session: nox.Session) -> None:
+    session.run_install(
+        "uv",
+        "sync",
+        "--no-default-groups",
+        "--group=test",
+        "--no-install-project",
+    )
+    session.run_install("uv", "pip", "install", "--upgrade", ".")
+
+    session.run("pytest", *session.posargs)
+
+
+@contextmanager
+def restore_file(path: str) -> Iterator[None]:
+    with open(path, "rb") as f:
+        original = f.read()
+    try:
+        yield
+    finally:
+        with open(path, "wb") as f:
+            f.write(original)
